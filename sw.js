e54e2f4085d4eee3a7fcb3381692832a1e45f3c1
Ankush Saggu <ankush22004@gmail.com>
2026-03-09 18:16:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
+const CACHE="pinksnake-games";
+
+const files=[
+"/",
+"/gameplay/",
+"/gameplay/index.html"
+];
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>{
+return cache.addAll(files);
+})
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res || fetch(e.request);
+})
+);
+});
 
